f530b59fbc4092788e85e9576ac8413fd3dfe240
Jun Nguyen <jun.nguyen@pnnl.gov>
2023-05-10 10:59:12 -0700

    Updated info, fixed popups

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 function closeModal(popup) {
-    document.getElementById(popup).classList.add('hidden');
+    if (popup === 'filters') {
+        document.getElementById(popup).classList.add('hidden-filters');
+    } else {
+        document.getElementById(popup).classList.add('hidden');
+    }
     document.getElementById(popup).style.backgroundColor = '#152E53';
 }
 
 function openModal(popup) {
-    document.getElementById(popup).classList.remove('hidden');
+    if (popup === 'filters') {
+        document.getElementById(popup).classList.remove('hidden-filters');
+    } else {
+        document.getElementById(popup).classList.remove('hidden');
+    }
     document.getElementById(popup).style.backgroundColor = '#FFFAFA';
 }
 
 function makeActive() {
     this.classList.add('active');
+}
+
+function createMap() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+
+    let map = new mapboxgl.Map({
+        container: 'map-object',
+        style: 'mapbox://styles/mapbox/dark-v10',
+        minZoom: 1,
+        maxZoom: 10,
+        zoom: 4,
+        center: [-98, 41.8]
+    });
+
+    map.on('load', () => {
+        map.addSource('basemap-tiles', {
+            'type': 'raster',
+            'tiles': [
+                'tiles/newattempt/{z}/{x}/{y}.png'
+            ],
+            'tileSize': 256,
+            'attribution': 'Map tiles designed by Ivette Ivanov</a>'
+        });
+    });
+
+    let legend = document.getElementById("legend");
 }
