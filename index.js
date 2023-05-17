bcfa3520fabf82cc94704bd5b5a22a1b530449e4
Jun Nguyen <soshiyo@uw.edu>
2023-05-16 21:53:04 -0700

    Worked on adding layers

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function clearFilters() {
     }
 }
 
-function createMap() {
+async function createMap() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
 
     let map = new mapboxgl.Map({
@@ -41,26 +41,100 @@ function createMap() {
         center: [-98, 41.8]
     });
 
+    let data = await getData();
+    console.log(data);
+
     map.on('load', () => {
-        map.addSource('basemap-tiles', {
-            'type': 'raster',
-            'tiles': [
-                'tiles/border/{z}/{x}/{y}.png'
-            ],
-            'tileSize': 256,
-            'attribution': 'Map tiles designed by Ivette Ivanov</a>'
+        // map.addSource('basemap-tiles', {
+        //     'type': 'raster',
+        //     'tiles': [
+        //         'tiles/border/{z}/{x}/{y}.png'
+        //     ],
+        //     'tileSize': 256,
+        //     'attribution': 'Map tiles designed by Ivette Ivanov</a>'
+        // });
+        // map.addLayer({
+        //     'id': 'border',
+        //     'type': 'raster',
+        //     'layout': {
+        //         'visibility': 'none'
+        //     },
+        //     'source': 'basemap-tiles',
+        //     'minzoom': 1,
+        //     'maxzoom': 10
+        // });
+
+        // map.addSource('distances', {
+        //     'type': 'raster',
+        //     'tiles': [
+        //         'tiles/distance/{z}/{x}/{y}.png'
+        //     ],
+        //     'tileSize': 256,
+        //     'attribution': 'Map tiles designed by Ivette Ivanov</a>'
+        // });
+        // map.addLayer({
+        //     'id': 'distance',
+        //     'type': 'raster',
+        //     'layout': {
+        //         'visibility': 'none'
+        //     },
+        //     'source': 'distances',
+        //     'minzoom': 1,
+        //     'maxzoom': 10
+        // });
+        map.addSource('driving', {
+            'type': 'geojson',
+            'data': {
+                'type': 'FeatureCollection',
+                'features': data
+            }
         });
+
         map.addLayer({
-            'id': 'border',
-            'type': 'raster',
+            'id': 'driving',
+            'type': 'symbol',
+            'source': 'driving',
             'layout': {
-                'visibility': 'none'
-            },
-            'source': 'basemap-tiles',
-            'minzoom': 1,
-            'maxzoom': 10
+                'icon-image': ['get', 'icon'],
+                'icon-allow-overlap': true
+            }
         });
     });
+
+    map.on('click', 'places', (e) => {
+        // Copy coordinates array.
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.description;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    });
+
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'places', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'places', () => {
+        map.getCanvas().style.cursor = '';
+    });
+}
+
+async function getData() {
+    const response = await fetch('./data/driving.geojson')
+        .then(res => res.json())
+        .catch(error => console.log(error));
+    return response;
 }
 
 function handleClick(selection) {
